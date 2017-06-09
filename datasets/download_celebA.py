a69f5cd0c1c1925ab17fe63b77083dc52afa6666
Arthur Goldberg <arthur.c.goldberg@gmail.com>
2017-06-09 17:02:16 +0100

    Create download_celebA.py

--- datasets/download_celebA.py
+++ datasets/download_celebA.py
@@ -1,16 +1,20 @@
 """
+Author: Akash Rana (github.com/akash9182)
+
 Modification of
 - https://github.com/carpedm20/DCGAN-tensorflow/blob/master/download.py
 - http://stackoverflow.com/a/39225039
 -
 """
-from __future__ import print_function
+
 import os
 import zipfile
 import requests
 import subprocess
 from tqdm import tqdm
 from collections import OrderedDict
+
+base_path = './'
 
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
@@ -70,48 +74,7 @@ def download_celeb_a(base_path):
     os.rename(os.path.join(base_path, "img_align_celeba"), images_path)
     os.remove(save_path)
 
-def prepare_data_dir(path = './dataset'):
-    if not os.path.exists(path):
-        os.mkdir(path)
 
-# check, if file exists, make link
-def check_link(in_dir, basename, out_dir):
-    in_file = os.path.join(in_dir, basename)
-    if os.path.exists(in_file):
-        link_file = os.path.join(out_dir, basename)
-        rel_link = os.path.relpath(in_file, out_dir)
-        os.symlink(rel_link, link_file)
-
-def add_splits(base_path):
-    data_path = os.path.join(base_path, 'CelebA')
-    images_path = os.path.join(data_path, 'images')
-    train_dir = os.path.join(data_path, 'splits', 'train')
-    valid_dir = os.path.join(data_path, 'splits', 'valid')
-    test_dir = os.path.join(data_path, 'splits', 'test')
-    if not os.path.exists(train_dir):
-        os.makedirs(train_dir)
-    if not os.path.exists(valid_dir):
-        os.makedirs(valid_dir)
-    if not os.path.exists(test_dir):
-        os.makedirs(test_dir)
-
-    # these constants based on the standard CelebA splits
-    NUM_EXAMPLES = 202599
-    TRAIN_STOP = 162770
-    VALID_STOP = 182637
-
-    for i in range(0, TRAIN_STOP):
-        basename = "{:06d}.jpg".format(i+1)
-        check_link(images_path, basename, train_dir)
-    for i in range(TRAIN_STOP, VALID_STOP):
-        basename = "{:06d}.jpg".format(i+1)
-        check_link(images_path, basename, valid_dir)
-    for i in range(VALID_STOP, NUM_EXAMPLES):
-        basename = "{:06d}.jpg".format(i+1)
-        check_link(images_path, basename, test_dir)
-
+        
 if __name__ == '__main__':
-    base_path = './dataset'
-    prepare_data_dir()
     download_celeb_a(base_path)
-    add_splits(base_path)
